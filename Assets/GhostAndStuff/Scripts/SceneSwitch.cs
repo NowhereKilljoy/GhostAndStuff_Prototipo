6968960bef60347e25c8b4278d66118e14b5203a
AlejandroOrtega28 <157633383+AlejandroOrtega28@users.noreply.github.com>
2024-09-06 00:59:40 -0600

    Respawn

---
+++ Assets/GhostAndStuff/Scripts/SceneSwitch.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
