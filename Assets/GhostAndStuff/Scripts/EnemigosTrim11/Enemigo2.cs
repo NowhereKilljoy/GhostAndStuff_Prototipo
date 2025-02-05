ff23c44191f8795086f8dc8425ea994ee47e69fd
NowhereKilljoy <148791213+NowhereKilljoy@users.noreply.github.com>
2025-02-05 11:55:04 -0600

    enemigo con animaciones

---
+++ Assets/GhostAndStuff/Scripts/EnemigosTrim11/Enemigo2.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemigo2 : MonoBehaviour
+{
+    public int rutina;
+    public float cronometro;
+    public Animator ani;
+    public Quaternion angulo;
+    public float grado;
+
+    //objetivo que detectara/perseguira el enemigo
+    public GameObject target;
+    //boleano del enemigo atacando
+    public bool atacando;
+    void Start()
+    {
+        ani = GetComponent<Animator>();
+        target = GameObject.Find("PlayerArmature");
+    }
+
+    public void Comportamiento_Enemigo()
+    {
+        if (Vector3.Distance(transform.position, target.transform.position) > 5)
+        {
+            ani.SetBool("run", false);
+            cronometro += 1 * Time.deltaTime;
+            if (cronometro >= 4)
+            {
+                rutina = Random.Range(0, 2);
+                cronometro = 0;
+            }
+            switch (rutina)
+            {
+                case 0:
+
+                    ani.SetBool("walk", false);
+                    break;
+
+                case 1:
+                    grado = Random.Range(0, 360);
+                    angulo = Quaternion.Euler(0, grado, 0);
+                    rutina++;
+                    break;
+
+                case 2:
+                    transform.rotation = Quaternion.RotateTowards(transform.rotation, angulo, 0.5f);
+                    transform.Translate(Vector3.forward * 1 * Time.deltaTime);
+                    ani.SetBool("walk", true);
+                    break;
+            }
+        }
+        else
+        {
+          if (Vector3.Distance(transform.position, target.transform.position) > 1 && !atacando)
+            {
+                var lookPos = target.transform.position - transform.position;
+                lookPos.y = 0;
+                var rotation = Quaternion.LookRotation(lookPos);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 2);
+                ani.SetBool("walk", false);
+
+                ani.SetBool("run", true);
+                transform.Translate(Vector3.forward * 2 * Time.deltaTime);
+
+                ani.SetBool("attack", false);
+            }
+            else
+            {
+                ani.SetBool("walk", false);
+                ani.SetBool("run", false);
+
+                ani.SetBool("attack", true);
+                atacando = true;
+
+
+            }
+        }
+
+
+    }
+
+    public void Final_Ani()
+    {
+        ani.SetBool("attack", false);
+        atacando = false;
+    
+    }
+
+
+
+
+
+
+
+
+private void Update()
+    {
+        Comportamiento_Enemigo();
+    }
+}
