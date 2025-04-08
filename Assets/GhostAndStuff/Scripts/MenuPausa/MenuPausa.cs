199ca6bcdb7e01fdef2a0c7856ef4897098f6dd4
Salud3 <78719641+Salud3@users.noreply.github.com>
2025-04-07 21:54:03 -0600

    Revert "Menus funcionando con control"
    This reverts commit 2a5668f966eebf52a8f1220b53ec6b9e942fcf78.

--- Assets/GhostAndStuff/Scripts/MenuPausa/MenuPausa.cs
+++ Assets/GhostAndStuff/Scripts/MenuPausa/MenuPausa.cs
@@ -8,16 +8,15 @@ public class MenuPausa : MonoBehaviour
     public GameObject ObjetoMenuPausa;
     public bool Pausa = false;
     public GameObject MenuSalir;
-
     void Start()
     {
-
+        
     }
 
+    
     void Update()
     {
-        // Verifica si se presiona la tecla ESC o el botón 7 del joystick (sin necesidad del Input Manager)
-        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.JoystickButton7))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (Pausa == false)
             {
@@ -27,28 +26,27 @@ public class MenuPausa : MonoBehaviour
                 Cursor.visible = true;
                 Cursor.lockState = CursorLockMode.None;
             }
+            
             else if (Pausa == true)
             {
                 Resumir();
             }
+
         }
     }
-
     public void Resumir()
     {
         ObjetoMenuPausa.SetActive(false);
         MenuSalir.SetActive(false);
-        Pausa = false;
+       Pausa = false;
         Time.timeScale = 1;
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
-
-    public void IrAlMenu(string NombreMenu)
+    public void IrAlMenu (string NombreMenu) 
     {
-        SceneManager.LoadScene(NombreMenu);
+    SceneManager.LoadScene(NombreMenu);
     }
-
     public void SalirDelJuego()
     {
         Application.Quit();
