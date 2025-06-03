ef2cc121d2ff0ada526028d845b30ae906786497
NowhereKilljoy <148791213+NowhereKilljoy@users.noreply.github.com>
2025-06-03 10:00:16 -0600

    cambios semi finales pre build

--- Assets/GhostAndStuff/Scripts/Respawn/CheckPoint.cs
+++ Assets/GhostAndStuff/Scripts/Respawn/CheckPoint.cs
@@ -1,28 +1,38 @@
 using UnityEngine;
 
 
+
+
 public class CheckPoint : MonoBehaviour
 {
     public GameObject onSpawner;
     public GameObject offSpawner;
 
-
+    private Animator onAnimator;
 
     private void Start()
     {
-
+        if (onSpawner != null)
+        {
+            onAnimator = onSpawner.GetComponent<Animator>();
+        }
     }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-
             RespawnManager respawnManager = FindObjectOfType<RespawnManager>();
             respawnManager.UpdateRespawnPoint(this.transform);
             Debug.Log("Checkpoint alcanzado en: " + transform.name);
 
             onSpawner.SetActive(true);
             offSpawner.SetActive(false);
+
+            if (onAnimator != null)
+            {
+                onAnimator.Play("ActivoLoop"); // nombre exacto del estado en el Animator
+            }
         }
     }
 }
